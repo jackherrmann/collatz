82add891468ce1efe26d89bc211c0438f44d9929
Jack Herrmann <jackherrmann111@gmail.com>
2021-08-16 22:42:04 -0400

    test

---
+++ collatz/src/main/java/com/jack/collatz/MaxBigInt.java
@@ -0,0 +1,27 @@
+package com.jack.collatz;
+import java.math.BigInteger; 
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println(BigInteger.TEN.pow(20)); 
+        System.out.println(BigInteger.TEN.pow(40));
+        System.out.println(BigInteger.TEN.pow(60));
+        System.out.println(BigInteger.TEN.pow(80));
+        System.out.println(BigInteger.TEN.pow(100));
+        System.out.println(BigInteger.TEN.pow(150));
+        System.out.println(BigInteger.TEN.pow(200));
+        System.out.println(BigInteger.TEN.pow(300));
+        System.out.println(BigInteger.TEN.pow(400));
+        System.out.println(BigInteger.TEN.pow(500));
+        System.out.println(BigInteger.TEN.pow(1000));
+
+
+    }
+}
