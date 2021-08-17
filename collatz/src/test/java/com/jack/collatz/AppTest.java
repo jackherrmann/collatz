7a8c16cb710046dbad45f3c960d613f794880a0b
Jack Herrmann <jackherrmann111@gmail.com>
2021-08-16 20:39:20 -0400

    first commit with empty maven repo

---
+++ collatz/src/test/java/com/jack/collatz/AppTest.java
@@ -0,0 +1,20 @@
+package com.jack.collatz;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
