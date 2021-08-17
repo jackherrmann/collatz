4e4fb4d034fe670c2c6fb4ff60703f2fb8220ec8
Jack Herrmann <jackherrmann111@gmail.com>
2021-08-16 21:09:18 -0400

    added logic

--- collatz/src/main/java/com/jack/collatz/App.java
+++ collatz/src/main/java/com/jack/collatz/App.java
@@ -1,4 +1,5 @@
 package com.jack.collatz;
+import java.math.BigInteger; 
 
 /**
  * Hello world!
@@ -8,6 +9,39 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        BigInteger iteration = BigInteger.TWO; 
+        BigInteger highest = BigInteger.TWO; 
+        BigInteger stopPoint = BigInteger.TEN.pow(6);
+
+        BigInteger three = BigInteger.ONE.add(BigInteger.TWO); 
+
+        System.out.println("Starting"); 
+
+        while (iteration.max(stopPoint).equals(iteration)) {
+
+            BigInteger result = BigInteger.ZERO.add(iteration); 
+
+            while (result.max(iteration).equals(result)) {
+                // if highest
+                if (result.max(highest).equals(result)) {
+                    highest = result; 
+                }
+                // if even
+                if (result.mod(BigInteger.TWO).equals(BigInteger.ONE)) {
+                    result = result.multiply(three); 
+                    result = result.add(BigInteger.ONE);   
+                } 
+                // if odd
+                else {
+                    result = result.divide(BigInteger.TWO); 
+                }
+
+            }
+
+            iteration = iteration.add(BigInteger.ONE); 
+
+        } 
+
+        System.out.println("Finished"); 
     }
 }
